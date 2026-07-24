172110ff94a5022a4c1aec5287ec215f4e38ba90
tommyliverani <43218487+tommyliverani@users.noreply.github.com>
2026-07-24 06:42:31 +0000

    feat: render templates for bucket5

---
+++ resources/bucket5/bucket/main.tf
@@ -0,0 +1,14 @@
+module "s3" {
+  source = "github.com/tommyliverani/highway-to-cloud//building_blocks/s3_bucket_module?ref=main"
+
+  bucket_name           = var.bucket_name
+  force_destroy         = var.force_destroy
+  enable_server_side_encryption = var.enable_server_side_encryption
+  kms_key               = var.kms_key
+  kms_sse_alg           = var.kms_sse_alg
+  object_lock_enabled   = var.object_lock_enabled
+  object_lock_retention_type = var.object_lock_enabled ? var.object_lock_retention_type : null
+  object_lock_retention_days = var.object_lock_enabled ? var.object_lock_retention_days : null
+  tags = var.tags
+}
+
